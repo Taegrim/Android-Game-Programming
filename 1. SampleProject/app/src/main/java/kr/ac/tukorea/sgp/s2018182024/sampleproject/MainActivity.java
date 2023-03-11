ee37d5cccace5e0be08b582e8cc45f2a410980cf
Lee Dong Hyun <aopo3948@gmail.com>
2023-03-11 19:22:06 +0900

    Sample Android Project
    안드로이드 게임 프로그래밍 첫 과제
    화면에 학번과 이름 띄우기

---
+++ 1. SampleProject/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/sampleproject/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.sgp.s2018182024.sampleproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
