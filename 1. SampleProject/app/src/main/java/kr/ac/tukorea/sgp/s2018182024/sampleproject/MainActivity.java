12a07f045b557700f2f3ca357e23bdd7f0dc5cca
Lee Dong Hyun <aopo3948@gmail.com>
2023-03-13 15:09:12 +0900

    버튼 클릭 이벤트 연결
    자기 자신에게 클릭을 알리는 함수를 만들어서
    버튼 클릭 시 문자열이 변경되도록 함

--- 1. SampleProject/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/sampleproject/MainActivity.java
+++ 1. SampleProject/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/sampleproject/MainActivity.java
@@ -3,15 +3,23 @@ package kr.ac.tukorea.sgp.s2018182024.sampleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.changeStringBtn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
         TextView tv = findViewById(R.id.snumTextView);
         tv.setText("새로운 문자열");
     }
