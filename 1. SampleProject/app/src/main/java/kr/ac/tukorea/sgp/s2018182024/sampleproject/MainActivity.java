2ad337a96aba91f4a865e7aed3fb3e9497dcf4d6
Lee Dong Hyun <aopo3948@gmail.com>
2023-03-13 14:55:47 +0900

    Runtime onCreate 에서 텍스트 변경
    app이 시작될 때 텍스트를 변경하도록 함
    findViewById 로 객체를 받아오고 해당 객체의 함수를 호출해 변경함

--- 1. SampleProject/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/sampleproject/MainActivity.java
+++ 1. SampleProject/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/sampleproject/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.tukorea.sgp.s2018182024.sampleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.snumTextView);
+        tv.setText("새로운 문자열");
     }
 }
