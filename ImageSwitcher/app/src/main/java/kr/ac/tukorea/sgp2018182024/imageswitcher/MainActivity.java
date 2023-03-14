908e30747627aa6508b7fb95625c163f09f63e87
Lee Dong Hyun <aopo3948@gmail.com>
2023-03-14 14:36:17 +0900

    Log 메세지 출력
    Logcat 에 Log 레벨, 태그, 문자열을 이용해 로그 메세지 출력

--- ImageSwitcher/app/src/main/java/kr/ac/tukorea/sgp2018182024/imageswitcher/MainActivity.java
+++ ImageSwitcher/app/src/main/java/kr/ac/tukorea/sgp2018182024/imageswitcher/MainActivity.java
@@ -3,12 +3,25 @@ package kr.ac.tukorea.sgp2018182024.imageswitcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnPrev(View view) {
+        Log.d(TAG, "Prev Button Click");
+    }
+
+    public void onBtnNext(View view) {
+        Log.d(TAG, "Next Button Click");
     }
 }
