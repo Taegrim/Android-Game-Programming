e03f720021876d902bd440ad9462f5bc11161094
LEE DONG HYUN <aopo3948@gmail.com>
2023-03-20 21:06:45 +0900

    카드 뒤집기, 같은 이미지 찾기, 선택횟수 출력
    시작 시 뒷면으로 시작하도록 변경
    리소스의 id를 배열에 담아서 index를 이용해 해당 리소스 id 를 찾을 수 있도록 함
    카드 뒤집을 때 같은 카드이면 뒤집었던 카드랑 같이 사라지도록 함
    카드를 선택할 때마다 뒤집은 횟수 Text로 보여줌

--- 3. MemoryGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/memorygame/MainActivity.java
+++ 3. MemoryGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/memorygame/MainActivity.java
@@ -5,23 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private int flips;
+    private TextView flipsTextView;
 
-    private static final int BUTTON_IDS[] = new int[] {
+    private static final int[] BUTTON_IDS = new int[] {
             R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
             R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
             R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
             R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33,
     };
 
+    private static final int[] RESOURCE_IDS = new int[]{
+            R.mipmap.card_2c,R.mipmap.card_3d,R.mipmap.card_4h,R.mipmap.card_5s,
+            R.mipmap.card_as,R.mipmap.card_qh,R.mipmap.card_jc,R.mipmap.card_kd,
+            R.mipmap.card_2c,R.mipmap.card_3d,R.mipmap.card_4h,R.mipmap.card_5s,
+            R.mipmap.card_as,R.mipmap.card_qh,R.mipmap.card_jc,R.mipmap.card_kd,
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flipsTextView = findViewById(R.id.flipsTextView);
     }
 
     // 버튼 id, index 를 담는 해시맵
@@ -33,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
             idMap.put(BUTTON_IDS[i], i);
         }
     }
-    private int getCardIndex(View view){
-        Integer index = idMap.get(view.getId());
+    private int getCardIndex(int id){
+        Integer index = idMap.get(id);
         if(null == index){
             Log.e(TAG, "Not Defined Card Index!");
             return -1;
@@ -42,8 +55,42 @@ public class MainActivity extends AppCompatActivity {
         return index;
     }
 
+    private ImageButton prevImageButton;
+
+    private void SetFlips(int flips){
+        this.flips = flips;
+        flipsTextView.setText("Flips : " + flips);
+    }
+
     public void onBtnCard(View view) {
-        int id = getCardIndex(view);
-        Log.d(TAG, "Index : " + id);
+        int index = getCardIndex(view.getId());
+        Log.d(TAG, "Index : " + index);
+
+        ImageButton btn = (ImageButton) view;
+        if(btn == prevImageButton){
+            return;
+        }
+
+        SetFlips(this.flips + 1);
+        int resId = RESOURCE_IDS[index];
+        btn.setImageResource(resId);
+
+        if(null != prevImageButton){
+            int prevIndex = getCardIndex(prevImageButton.getId());
+            if(resId == RESOURCE_IDS[prevIndex]){
+                btn.setVisibility(View.INVISIBLE);
+                prevImageButton.setVisibility(View.INVISIBLE);
+                prevImageButton = null;
+                return;
+            }
+            else {
+                prevImageButton.setImageResource(R.mipmap.card_blue_back);
+            }
+        }
+        prevImageButton = btn;
+    }
+
+    public void onBtnRestart(View view) {
+
     }
 }
