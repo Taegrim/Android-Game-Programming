fc043e6e9c54ebf14d47d85c5e79dd3fb8a5e2ca
Lee Dong Hyun <aopo3948@gmail.com>
2023-03-26 16:41:56 +0900

    이름을 입력하면 반영되도록 EditText 추가
    EditText에 입력된 이름을 받아 버튼을 클릭하면 해당 이름이 반영되도록 함
    입력하지 않았을 경우 NoName 이 출력됨

--- 4. MoreControls/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/morecontrols/MainActivity.java
+++ 4. MoreControls/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/morecontrols/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox goodProgrammerCheckbox;
     private TextView outputTextView;
     private TextView emojiTextView;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +24,22 @@ public class MainActivity extends AppCompatActivity {
         goodProgrammerCheckbox = findViewById(R.id.goodProgrammerCheckbox);
         outputTextView = findViewById(R.id.outputTextView);
         emojiTextView = findViewById(R.id.emojiTextView);
+        nameEditText = findViewById(R.id.nameEditText);
     }
 
     public void onBtnDoIt(View view) {
         Log.d(TAG, "Do It!");
-        int stringResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_for_good : R.string.pay_for_not_good;
-        outputTextView.setText(getString(stringResId));
+        int stringResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_for_good_fmt : R.string.pay_for_not_good_fmt;
+        String name = nameEditText.getText().toString();
+        if(0 == name.trim().length()){
+            name = "NoName";
+        }
+        String text = getString(stringResId, name);
+        outputTextView.setText(text);
     }
 
     public void onCheckBoxGoodProgrammer(View view) {
-        int emojiResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_good_emoji : R.string.pay_not_goot_emoji;
+        int emojiResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_good_emoji : R.string.pay_not_good_emoji;
         emojiTextView.setText(getString(emojiResId));
     }
 }
