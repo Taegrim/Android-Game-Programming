1b308d2f9cb453510b3dd61aa77d1fe30d61147d
Lee Dong Hyun <aopo3948@gmail.com>
2023-03-26 16:31:27 +0900

    체크 박스 클릭에 반응하는 이모티콘 텍스트 뷰 추가
    체크 박스에 onClick 이벤트를 추가하여 클릭에 반응하는
    이모티콘 텍스트 뷰 추가

--- 4. MoreControls/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/morecontrols/MainActivity.java
+++ 4. MoreControls/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/morecontrols/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private CheckBox goodProgrammerCheckbox;
     private TextView outputTextView;
+    private TextView emojiTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
         goodProgrammerCheckbox = findViewById(R.id.goodProgrammerCheckbox);
         outputTextView = findViewById(R.id.outputTextView);
+        emojiTextView = findViewById(R.id.emojiTextView);
     }
 
     public void onBtnDoIt(View view) {
         Log.d(TAG, "Do It!");
         int stringResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_for_good : R.string.pay_for_not_good;
         outputTextView.setText(getString(stringResId));
+    }
+
+    public void onCheckBoxGoodProgrammer(View view) {
+        int emojiResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_good_emoji : R.string.pay_not_goot_emoji;
+        emojiTextView.setText(getString(emojiResId));
     }
 }
