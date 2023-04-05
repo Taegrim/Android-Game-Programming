64f50527586c315da8252a9fa0f7f75099a91425
Lee Dong Hyun <aopo3948@gmail.com>
2023-04-05 15:50:43 +0900

    다른 액티비티에 BallView 연결, BallView 에서 Text 출력, 버튼 클릭시 다른 액티비티로 전환
    Another Activity 에 BallView 연결
    BallView 에선 Text Draw
    Main Activity 에서 New Activity 버튼 클릭 시 Another Activity 로 전환되도록 함

---
+++ 4. MoreControls/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/morecontrols/BallView.java
@@ -0,0 +1,59 @@
+package kr.ac.tukorea.sgp.s2018182024.morecontrols;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class BallView extends View {
+    private static final String TAG = BallView.class.getSimpleName();
+    private Paint paint = new Paint();
+
+    public BallView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public BallView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public BallView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        paint.setTextSize(100);
+        paint.setColor(Color.BLUE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        canvas.drawRect(paddingLeft, paddingTop, paddingLeft + contentWidth, paddingTop +contentHeight, paint);
+        canvas.drawText("Hello Custon View!", paddingLeft + 100, paddingTop + 100, paint);
+    }
+}
