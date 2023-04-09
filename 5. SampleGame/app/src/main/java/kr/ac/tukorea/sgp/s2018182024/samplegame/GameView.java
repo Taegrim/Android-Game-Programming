b2bae27867874c3039fe73e476c9372988ee6709
Lee Dong Hyun <aopo3948@gmail.com>
2023-04-09 22:36:40 +0900

    SampleGame 프로젝트 생성
    SampleGame 을 위한 프로젝트 생성 및 GameView 추가

---
+++ 5. SampleGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/samplegame/GameView.java
@@ -0,0 +1,41 @@
+package kr.ac.tukorea.sgp.s2018182024.samplegame;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
