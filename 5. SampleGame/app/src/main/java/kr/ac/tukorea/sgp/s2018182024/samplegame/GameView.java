7e975f756b4d996a7f02abae69cb9afec2656065
Lee Dong Hyun <aopo3948@gmail.com>
2023-04-09 22:48:02 +0900

    사이즈가 변할 때 scale 을 정하고 공을 화면의 정중앙에 그리기
    사이즈가 변할 때 scale 의 값을 폭의 1/10 으로 정함
    공은 폭의 1/4 가 되도록 하고 화면의 정중앙에 그림

--- 5. SampleGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/samplegame/GameView.java
+++ 5. SampleGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/samplegame/GameView.java
@@ -1,10 +1,14 @@
 package kr.ac.tukorea.sgp.s2018182024.samplegame;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
@@ -14,6 +18,11 @@ import android.view.View;
  * TODO: document your custom view class.
  */
 public class GameView extends View {
+    private static final String TAG = GameView.class.getSimpleName();
+    private Bitmap soccerBitmap;
+    private RectF soccerRect = new RectF();
+    private float scale;
+
     public GameView(Context context) {
         super(context);
         init(null, 0);
@@ -30,12 +39,27 @@ public class GameView extends View {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
+        Resources res = getResources();
+        soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
 
+        float cx = 5.0f;
+        float cy = 7.0f;
+        float radius = 1.25f;
+        soccerRect.set(cx - radius, cy - radius, cx + radius, cy + radius);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+
+        scale = w / 10.f;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.scale(scale, scale);
+        canvas.drawBitmap(soccerBitmap, null, soccerRect, null);
     }
 }
