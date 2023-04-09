4630331848e77c6c2a281608b3b98cbebc17f104
Lee Dong Hyun <aopo3948@gmail.com>
2023-04-09 23:39:55 +0900

    Ball 클래스를 만들고 ArrayList 에 담아서 ball 들을 관리
    Ball 클래스가 Bitmap 을 공유하여 저장하도록 함
    객체별로 ball rect를 가지게 하고 이를 그리고 offset 처리를 함
    Ball를 담는 ArrayList 를 생성하고 이를 이용해 ball 들을 처리함

---
+++ 5. SampleGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/samplegame/Ball.java
@@ -0,0 +1,50 @@
+package kr.ac.tukorea.sgp.s2018182024.samplegame;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.RectF;
+
+public class Ball {
+    private static Bitmap bitmap;
+    private RectF rect = new RectF();
+    private float dx, dy;
+
+    public Ball(float dx, float dy){
+        this.dx = dx;
+        this.dy = dy;
+        rect.set(0, 0, 2.5f, 2.5f);
+    }
+
+    public static void setBitmap(Bitmap bitmap) {
+        Ball.bitmap = bitmap;
+    }
+
+    public void update() {
+        rect.offset(dx, dy);
+        if(dx > 0){
+            if(rect.right > 10.0f){
+                dx = -dx;
+            }
+        }
+        else{
+            if(rect.left < 0){
+                dx = -dx;
+            }
+        }
+
+        if(dy > 0){
+            if(rect.bottom > 15.0f){
+                dy = -dy;
+            }
+        }
+        else{
+            if(rect.top < 0){
+                dy = -dy;
+            }
+        }
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(bitmap, null, rect, null);
+    }
+}
--- 5. SampleGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/samplegame/GameView.java
+++ 5. SampleGame/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/samplegame/GameView.java
@@ -2,28 +2,22 @@ package kr.ac.tukorea.sgp.s2018182024.samplegame;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Choreographer;
 import android.view.View;
+
+import java.util.ArrayList;
 
 /**
  * TODO: document your custom view class.
  */
 public class GameView extends View implements Choreographer.FrameCallback {
     private static final String TAG = GameView.class.getSimpleName();
-    private Bitmap soccerBitmap;
-    private RectF soccerRect = new RectF();
-    private float ballDx, ballDy;
+    private ArrayList<Ball> balls = new ArrayList<>();
     private float scale;
 
     public GameView(Context context) {
@@ -43,14 +37,11 @@ public class GameView extends View implements Choreographer.FrameCallback {
 
     private void init(AttributeSet attrs, int defStyle) {
         Resources res = getResources();
-        soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+        Bitmap soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+        Ball.setBitmap(soccerBitmap);
 
-        float cx = 5.0f;
-        float cy = 7.0f;
-        float radius = 1.25f;
-        soccerRect.set(cx - radius, cy - radius, cx + radius, cy + radius);
-        ballDx = 0.04f;
-        ballDy = 0.06f;
+        balls.add(new Ball(0.04f, 0.06f));
+        balls.add(new Ball(0.075f, 0.056f));
 
         Choreographer.getInstance().postFrameCallback(this);
     }
@@ -72,27 +63,8 @@ public class GameView extends View implements Choreographer.FrameCallback {
     }
 
     private void update() {
-        soccerRect.offset(ballDx, ballDy);
-        if(ballDx > 0){
-            if(soccerRect.right > 10){
-                ballDx = -ballDx;
-            }
-        }
-        else{
-            if(soccerRect.left < 0){
-                ballDx = -ballDx;
-            }
-        }
-
-        if(ballDy > 0){
-            if(soccerRect.bottom > 15.0f){
-                ballDy = -ballDy;
-            }
-        }
-        else{
-            if(soccerRect.top < 0){
-                ballDy = -ballDy;
-            }
+        for(Ball ball : balls){
+            ball.update();
         }
     }
 
@@ -102,6 +74,8 @@ public class GameView extends View implements Choreographer.FrameCallback {
         super.onDraw(canvas);
 
         canvas.scale(scale, scale);
-        canvas.drawBitmap(soccerBitmap, null, soccerRect, null);
+        for(Ball ball : balls){
+            ball.draw(canvas);
+        }
     }
 }
