ebcf3d808b18b76ebe6a2e48e67782dc84e8c2df
Lee Dong Hyun <aopo3948@gmail.com>
2023-04-24 11:56:30 +0900

    타이틀에 이미지 추가 및 버튼 클릭 시 이동
    타이틀에 tree 이미지 추가
    하이 스코어 버튼 클릭 시 이동하도록 함

--- 6. DragonFlight/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/dragonflight/app/TitleActivity.java
+++ 6. DragonFlight/app/src/main/java/kr/ac/tukorea/sgp/s2018182024/dragonflight/app/TitleActivity.java
@@ -7,16 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 
 import kr.ac.tukorea.sgp.s2018182024.dragonflight.R;
+import kr.ac.tukorea.sgp.s2018182024.dragonflight.databinding.ActivityTitleBinding;
 
 public class TitleActivity extends AppCompatActivity {
+    private ActivityTitleBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title);
+        binding = ActivityTitleBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     public void onBtnStartGame(View view) {
         startActivity(new Intent(this, MainActivity.class));
+    }
+
+    private float progress;
+    public void onBtnProgress(View view) {
+        progress += 0.01f;
+
+        float x = -1 * binding.titleTree.getWidth() * progress;
+        binding.titleTree.setTranslationX(x);
     }
 }
